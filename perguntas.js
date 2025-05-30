d10352456963cce5a3a3b3b739933e2b84357cc5
mariaesa2 <maria.santos.alves07@escola.pr.gov.br>
2025-05-30 15:13:22 +0000

    finalizando

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Geografia',
+    'Quais são as principais formas de relevo?',
+    'Montanhas, planaltos, planícies e depressões.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a diferença entre clima e tempo?',
+    'Clima é o padrão do tempo ao longo dos anos, tempo é a condição atmosférica do momento.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são os principais tipos de clima no Brasil?',
+    'Equatorial, tropical, semiárido, subtropical e tropical de altitude.'
+)
