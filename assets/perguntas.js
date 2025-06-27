1e05b318c076a061fa0a934f0cfe0d2e1f7ea326
mariaesa2 <maria.santos.alves07@escola.pr.gov.br>
2025-06-27 11:16:25 +0000

    terminando

--- assets/perguntas.js
+++ assets/perguntas.js
@@ -2,16 +2,16 @@ criaCartao(
     'Matemática',
     'Qual é a equação que descreve a seguinte frase "um número x elevado a dois é igual a trinta e seis"',
     'É x^2=36 (x.x=36)'
-)
+);
 
 criaCartao(
     'Matemática',
     'Quais são as figuras geométricas que tem todos os lados iguais ',
     'Quadrado e triângulo escaleno'
-)
+);
 
 criaCartao(
     'Matemática',
     'Dona lais  ',
     'ffsffsyf'
-)
+);
